a382bc47491bb7c8400031293b9d03f75ca77702
Elias Brazanovich <elias.brazanovich@gmail.com>
2024-05-23 20:01:00 -0300

    colocando navbar en la pag

--- Manicura/src/App.jsx
+++ Manicura/src/App.jsx
@@ -1,9 +1,16 @@
 import "./App.css";
+import Boton from "./components/atoms/button.jsx";
+import Navbar from "./components/molecules/navbar/navbar.jsx";
 
 function App() {
   return (
     <>
+      <Navbar />
+      <div>asd</div>
+
       <h1>Hola amigos, titulo de manicura aqui</h1>
+      <Boton nameButton="Hola" />
+      <Boton nameButton="Click Me" />
     </>
   );
 }
