abd360cc5233d0d5051b939552b6c7855d3f8825
Lallana-Brian <lallanabn@gmail.com>
2025-02-22 05:57:43 -0300

    corregí errores del swiper y acomodé el texto con el botón centrados pero la fuente no me convence

--- Manicura/src/components/homeSwiper/HomeSwiper.jsx
+++ Manicura/src/components/homeSwiper/HomeSwiper.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react'; 
-import 'swiper/css'; 
-import 'swiper/css/navigation'; 
-import 'swiper/css/pagination'; 
-import './HomeSwiper.css'; 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Autoplay, EffectFade } from 'swiper/modules'; // Importa los módulos
+import 'swiper/css'; // Importa los estilos base de Swiper
+import 'swiper/css/autoplay'; // Importa los estilos para autoplay
+import './HomeSwiper.css'; // Tus estilos personalizados
 
 // Imágenes
 import bluenailImage from '../../assets/image/swiper/bluenail.jpg';
@@ -12,40 +12,32 @@ import yellownailImage from '../../assets/image/swiper/yellownail.jpg';
 
 const HomeSwiper = () => {
   const slidesData = [
-    {
-      imgSrc: bluenailImage,
-      alt: "Slide 1",
-    },
-    {
-      imgSrc: mixnailImage,
-      alt: "Slide 2",
-    },
-    {
-      imgSrc: yellownailImage,
-      alt: "Slide 3",
-    },
+    { imgSrc: bluenailImage, alt: 'Slide 1' },
+    { imgSrc: mixnailImage, alt: 'Slide 2' },
+    { imgSrc: yellownailImage, alt: 'Slide 3' },
   ];
 
   return (
     <div className="home-swiper-container">
-      
       {/* Sección fija encima del Swiper */}
       <div className="fixed-section">
-        <h1>Bienvenido a la tienda</h1>
-        <h2>Explora nuestras mejores ofertas y productos exclusivos</h2>
+        <h1>¿Estás lista<br />para tu<br />mejor<br />versión?</h1>
         <div className="buttons-container">
-          <button>Ver más ofertas</button>
-          <button>Explorar productos</button>
+          <button>Reserva una cita</button>
+          
         </div>
       </div>
 
       {/* Contenedor principal del Swiper */}
       <Swiper
+        modules={[ Autoplay ]} // Agrega los módulos aquí
         spaceBetween={0}
         slidesPerView={1}
         loop={true}
-        autoplay={{ delay: 1000 }}
-        effect="fade"
+        autoplay={{
+          delay: 3000, // Cambia de slide cada 3 segundos
+          disableOnInteraction: false, // No se detiene al interactuar
+        }}
       >
         {slidesData.map((slide, index) => (
           <SwiperSlide key={index}>
