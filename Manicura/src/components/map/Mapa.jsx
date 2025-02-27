bff25038a94f5678d63ca668ad8383503286fbca
Miguel <miguegomez2706@gmail.com>
2025-02-27 11:47:36 -0300

    Agregando ubicacion del local

---
+++ Manicura/src/components/map/Mapa.jsx
@@ -0,0 +1,38 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+// Importar la imagen del marcador rojo
+import redMarkerIcon from "../../assets/image/comunes/red-marker-icon.png"; // Asegúrate de ajustar la ruta
+
+// Configurar el icono personalizado
+const redIcon = L.icon({
+  iconUrl: redMarkerIcon,
+  iconSize: [30, 30], // Ajusta el tamaño del ícono (ancho, alto)
+  iconAnchor: [20, 12], // Ajusta el punto de anclaje del ícono
+  popupAnchor: [-3, -10], // Ajusta la posición del popup
+});
+
+const Mapa = () => {
+  const posicion = [-27.45129898657986, -58.99163777380161]; // Local de Romi
+
+  return (
+    <div className="h-[500px] w-full">
+      <MapContainer
+        center={posicion}
+        zoom={30}
+        className="h-full w-full rounded-lg"
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={posicion} icon={redIcon}>
+          <Popup>¡Aquí está el local! 📍</Popup>
+        </Marker>
+      </MapContainer>
+    </div>
+  );
+};
+
+export default Mapa;
--- Manicura/src/components/salonPage/SalonPage.jsx
+++ Manicura/src/components/salonPage/SalonPage.jsx
@@ -1,11 +1,12 @@
+import Mapa from "../map/Mapa.jsx";
 const SalonPage = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100 p-10">
+    <div className="min-h-screen flex flex-col bg-gray-100 p-10">
       <h1 className="text-4xl font-bold mb-6">Acerca de Nuestro Salón</h1>
       <p className="text-lg text-gray-700 max-w-2xl text-center">
-        En Only Nails, ofrecemos una experiencia única en el cuidado de tus uñas.
-        Con más de 30 años en la industria, nuestro equipo de expertos está
-        comprometido con la calidad y el bienestar de nuestros clientes.
+        En Only Nails, ofrecemos una experiencia única en el cuidado de tus
+        uñas. Con más de 30 años en la industria, nuestro equipo de expertos
+        está comprometido con la calidad y el bienestar de nuestros clientes.
       </p>
       <button
         className="mt-6 px-6 py-2 text-white bg-gray-800 rounded hover:bg-gray-700"
@@ -13,8 +14,22 @@ const SalonPage = () => {
       >
         Volver
       </button>
+      <div className="grid grid-cols-2 p-4">
+        <div>
+          <h1 className="flex text-2xl font-bold justify-center mb-4">
+            Ubicación del Local
+          </h1>
+          <div className="">
+            <Mapa />
+          </div>
+        </div>
+        <div className="flex items-center justify-center font-semibold ">
+          Datos: Necochea 307 OF 3, H3500 Resistencia, Chaco agregar mas datos
+        </div>
+      </div>
     </div>
   );
 };
 
 export default SalonPage;
+// const posicion = [-27.45129898657986, -58.99163777380161]; // Local de Romi
