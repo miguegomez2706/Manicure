70a40f709ad253523a281f6e1821e946fef58f0a
Lallana-Brian <lallanabn@gmail.com>
2025-03-04 21:42:43 -0300

    ahora el swiper y su contenido usa grid, todavia me falta trabajar en el responsive

--- Manicura/src/components/atoms/Btn.jsx
+++ Manicura/src/components/atoms/Btn.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 
-const Btn = ({ buttonText, type, rounded, onClick }) => {
+const Btn = ({ buttonText, type, rounded, onClick, css }) => {
   const baseStyles =
     "px-6 py-2 md:px-8 md:py-3 lg:px-10 lg:py-4 w-32 h-16 hover:bg-slate-700 flex text-nowrap justify-center items-center";
 
@@ -13,7 +13,7 @@ const Btn = ({ buttonText, type, rounded, onClick }) => {
 
   return (
     <button
-      className={`${style[type]} ${roundedStyle} ${baseStyles}`}
+      className={`${css}${style[type]} ${roundedStyle} ${baseStyles}`}
       onClick={onClick}  // Agregado el onClick aquí
     >
       {buttonText}
