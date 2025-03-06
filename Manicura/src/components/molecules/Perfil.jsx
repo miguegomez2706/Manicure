4bf40fece406dbd750ae89406988f8e8b971fe43
Miguel <miguegomez2706@gmail.com>
2025-03-06 11:52:19 -0300

    Agregando los perfiles de las personas

---
+++ Manicura/src/components/molecules/Perfil.jsx
@@ -0,0 +1,26 @@
+import Btn from "../atoms/Btn";
+import img1 from "../../assets/image/semipermanente/semi3.jpeg";
+
+const Commercial = () => {
+  return (
+    <div className="flex justify-center">
+      <div className="grid grid-cols-1 max-w-80 gap-4 border border-gray-200 rounded-lg overflow-hidden shadow-lg">
+        <div className="order-1 ">
+          <img
+            src={img1}
+            alt="Commercial Image"
+            className="h-56 md:h-80 lg:h-full w-full object-cover transition-transform duration-300 hover:scale-105 max-h-72"
+          />
+        </div>
+        <div className="flex flex-col justify-center items-center p-6 md:p-10 order-2 ">
+          <p className="font-rale text-base md:text-2xl lg:text-3xl text-center pb-3 xl:pb-6">
+            Mejores Precios
+          </p>
+          <Btn rounded={true} type={"primary"} buttonText={"Ver Precios!"} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Commercial;
--- Manicura/src/components/salonPage/SalonPage.jsx
+++ Manicura/src/components/salonPage/SalonPage.jsx
@@ -1,12 +1,13 @@
 // import { useNavigate } from "react-router-dom";
-import imgs from "../../assets/image/semipermanente/semi6.jpeg";
+// import imgs from "../../assets/image/semipermanente/semi6.jpeg";
 // import imgs from "../../assets/image/comunes/imagenLocal.jpeg";
-import Mapa from "../map/Mapa";
+// import Mapa from "../map/Mapa";
+import Perfil from "../molecules/Perfil";
 const SalonPage = () => {
   // const navigate = useNavigate();
   return (
     <div className="min-h-screen flex flex-col bg-gray-100 p-10">
-      <section className="grid grid-cols-2">
+      {/* <section className="grid grid-cols-2">
         <div className="flex flex-col items-center justify-center">
           <h1 className="text-4xl font-bold mb-6">Sobre Nostros</h1>
           <p className="text-lg text-gray-700 max-w-2xl">
@@ -44,31 +45,19 @@ const SalonPage = () => {
             Datos: Necochea 307 OF 3, H3500 Resistencia, Chaco agregar mas datos
           </div>
         </div>
-      </section>
+      </section> */}
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+        <div>
+          <Perfil />
+        </div>
+        <div>
+          <Perfil />
+        </div>
+        <div>
+          <Perfil />
+        </div>
+      </div>
     </div>
-
-    // <section className="min-h-screen">
-    //   <div className="flex ">
-    //     <div>
-    //       <h1 className="text-4xl font-bold mb-6">Acerca de Nuestro Salón</h1>
-    //       <p className="text-lg text-gray-700 max-w-2xl text-center">
-    //         En Only Nails, ofrecemos una experiencia única en el cuidado de tus
-    //         uñas. Con más de 30 años en la industria, nuestro equipo de expertos
-    //         está comprometido con la calidad y el bienestar de nuestros
-    //         clientes.
-    //       </p>
-    //       <button
-    //         className="mt-6 px-6 py-2 text-white bg-gray-800 rounded hover:bg-gray-700"
-    //         onClick={() => navigate("/")} // Para volver atrás
-    //       >
-    //         Volver
-    //       </button>
-    //     </div>
-    //     <div>
-    //       <img src={imgs} alt="" className="object-cover w-full p-10 h-150" />
-    //     </div>
-    //   </div>
-    // </section>
   );
 };
 
