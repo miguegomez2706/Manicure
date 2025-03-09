f6782550ed30f6df2ed731eb7586b9d7a924fefe
Lallana-Brian <lallanabn@gmail.com>
2025-03-08 23:08:15 -0300

    creacion de página de reserva, con calendario de React y mejora de codigo responsive con grid en el modal

--- Manicura/src/components/turns/ModalTurnos.jsx
+++ Manicura/src/components/turns/ModalTurnos.jsx
@@ -17,7 +17,7 @@ const ModalTurnos = ({ isOpen, onClose, servicios }) => {
           <h1 className="titulo">Servícios disponíbles.</h1>
           {servicios.map((servicio, index) => (
             <div key={index} className="servicio">
-              <h2>{servicio.nombre}</h2>
+              <h2>{servicio.nombre}</h2> 
               <hr className="servicio-linea" />
               <p>{servicio.descripcion}</p>
               <p>
