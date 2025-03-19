60bf1856557db04e56da04b098e7c573283c7266
Lallana-Brian <lallanabn@gmail.com>
2025-03-18 22:17:25 -0300

    pasando a tailwind

--- Manicura/src/components/homeSwiper/HomeSwiper.jsx
+++ Manicura/src/components/homeSwiper/HomeSwiper.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import "swiper/css";
-import "./HomeSwiper.css";
 import ModalTurnos from "../turns/ModalTurnos"; // Importamos la modal
 import servicios from "../turns/servicios"; // Importamos los servicios
 
@@ -20,26 +19,9 @@ const HomeSwiper = () => {
   ];
 
   return (
-    <div className="home-swiper-wrapper">
-      {" "}
-      {/* Nuevo contenedor */}
-      {/* Sección fija encima del Swiper */}
-      <div className="fixed-section">
-        <h1>
-          ¿Estás lista
-          <br />
-          para tu
-          <br />
-          mejor
-          <br />
-          versión?
-        </h1>
-        <div className="buttons-container">
-          <button onClick={() => setModalOpen(true)}>Reserva una cita</button>
-        </div>
-      </div>
+    <div className="relative w-full">
       {/* Contenedor del Swiper */}
-      <div className="home-swiper-container">
+      <div className="w-full h-[95vh] overflow-hidden flex items-center justify-center">
         <Swiper
           modules={[Autoplay]}
           spaceBetween={0}
@@ -53,15 +35,41 @@ const HomeSwiper = () => {
         >
           {slidesData.map((slide, index) => (
             <SwiperSlide key={index}>
-              <div className="home-swiper-slide">
-                <div className="image-section">
-                  <img src={slide.imgSrc} alt={slide.alt} />
+              <div className="flex items-center justify-center w-full h-full">
+                <div className="w-full h-full mx-24">
+                  <img
+                    src={slide.imgSrc}
+                    alt={slide.alt}
+                    className="w-full h-full object-cover"
+                  />
                 </div>
               </div>
             </SwiperSlide>
           ))}
         </Swiper>
       </div>
+
+      {/* Sección del título y botón debajo del Swiper */}
+      <div className="flex flex-col justify-center items-center sm:items-start sm:absolute sm:top-[15%] sm:left-[70%] sm:transform sm:-translate-x-1/2 sm:w-[20%] sm:z-10 sm:text-center p-4 sm:p-0 ">
+  <h1 className="font-rale text-4xl md:text-5xl lg:text-6xl xl:text-7xl text-black sm:text-white text-shadow-lg">
+    ¿Estás lista
+    <br />
+    para tu
+    <br />
+    mejor
+    <br />
+    versión?
+  </h1>
+        <div className="mt-5 sm:mt-8 flex justify-center w-full">
+          <button
+            onClick={() => setModalOpen(true)}
+          className="px-7 py-3 bg-button text-white cursor-pointer text-xl rounded-lg mt-8 sm:mt-4 border-2 border-solid border-gray-900 hover:bg-buttonHover hover:border-gray-900 bg-gray-900 hover:bg-transparent "
+          >
+            Reserva una cita
+          </button>
+        </div>
+      </div>
+
       {/* Modal */}
       <ModalTurnos
         isOpen={modalOpen}
